d571222e3ede9a13090ac4a5c34f24559d521796
Lidia Bernasconi <lidiabernasconi6@gmail.com>
2025-11-19 11:25:29 +0100

    19.11.2025-draft

--- tappa1/app.py
+++ tappa1/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Benvenuti</h1> nel tutorial Flask !!!"
+    return render_template("index.html")
 
+@app.route("/about")
+def about():
+    mess = "Contenuto del mio messaggio"
+    return render_template("index.html", messaggio=mess)
 
+@app.route("/classe/<string:nome>")
+def nome_classe(nome):
+    return f"Il nome è {nome}"
+
+@app.route("/classe/<int:anno>")
+def anno_classe(anno):
+    return f"Anno: {anno}"
 
 
 if __name__ == '__main__':
